45e992f571d1f13c53eead58b7da99907affcd45
GabrielDvPereira <gdavi.gd@gmail.com>
2022-12-21 21:52:28 -0300

    feat: add http helpers

---
+++ src/helpers/http-helpers.ts
@@ -0,0 +1,31 @@
+import { ServerError, UnauthorizedError } from '_/errors'
+import { HttpResponse } from '_/types/http'
+
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
+})
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
