570553392ac514b94d1cea790f6731fded70e1f6
GabrielDvPereira <gdavi.gd@gmail.com>
2023-01-06 15:54:54 -0300

    feat(controller): add controller type

--- src/types/controller.ts
+++ src/types/controller.ts
@@ -1,3 +1,4 @@
 import { HttpRequest, HttpResponse } from "./http";
 
+export type Controller<T> = Record<keyof T, ControllerMethod>
 export type ControllerMethod = (httpRequest: HttpRequest) => Promise<HttpResponse>
