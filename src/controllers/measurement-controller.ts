696fdbd7e3843a2fe996c20db955951d39daed7a
GabrielDvPereira <gdavi.gd@gmail.com>
2023-01-07 17:44:24 -0300

    refactor(totem-controller): add try catch and adapt database repository interface

--- src/controllers/measurement-controller.ts
+++ src/controllers/measurement-controller.ts
@@ -1,30 +1,34 @@
-import { ok, created, badRequest } from "_/helpers/http-helpers";
+import * as httpStatus from "_/helpers/http-helpers";
 import { mapBodyToMeasurement } from "_/helpers/map-body-to-measurement";
-import { DatabaseRepository } from "_/repositories/database";
-import { HttpRequest, HttpResponse, Controller } from "_/types";
+import { HttpRequest, HttpResponse, Controller, IDatabaseRepository } from "_/types";
 import { MeasurementDto, Totem } from "_/models"
-
-/** Record<keyof MeasurementController, ControllerMethods> makes sure every method in the controller class have the same params and return type */
-
 export class MeasurementController implements Controller<MeasurementController> {
 
     constructor(
-        private readonly measurementDatabaseRepository: DatabaseRepository,
-        private readonly totemDatabaseRepository: DatabaseRepository,
+        private readonly measurementDatabaseRepository: IDatabaseRepository,
+        private readonly totemDatabaseRepository: IDatabaseRepository,
     ){}
 
     async createMeasurement(httpRequest: HttpRequest<MeasurementDto>): Promise<HttpResponse> {
-        const { totem_id } = httpRequest.body
-        const totem = await this.totemDatabaseRepository.findOne<Totem>({_id: totem_id })
-        if(!totem) return badRequest(new Error("Totem não encontrado."))
+        try {
+            const { totem_id } = httpRequest.body
+            const totem = await this.totemDatabaseRepository.findOne<Totem>({id: totem_id})
+            if(!totem) return httpStatus.badRequest(new Error("Totem não encontrado."))
 
-        await this.measurementDatabaseRepository.create(mapBodyToMeasurement(httpRequest.body));
-        return created();
+            await this.measurementDatabaseRepository.create(mapBodyToMeasurement(httpRequest.body));
+            return httpStatus.created();
+        } catch(error){
+            return httpStatus.serverError(error)
+        }
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    async listMeasurement(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const measurements = await new DatabaseRepository('measurements').findAll();
-        return ok({measurements})
+    async listMeasurement(_: HttpRequest): Promise<HttpResponse> {
+        try {
+            const measurements = await this.measurementDatabaseRepository.findAll();
+            return httpStatus.ok({ measurements })
+        } catch(error){
+            return httpStatus.serverError(error)
+        }
     }
 }
