9c22393b9177e8a262e1ec1042181471d655f2e2
GabrielDvPereira <gdavi.gd@gmail.com>
2023-01-07 17:49:09 -0300

    refactor(datapase-repository): adjust generic type

--- src/types/database-repository.ts
+++ src/types/database-repository.ts
@@ -3,7 +3,7 @@ import { Model } from "./model"
 export interface IDatabaseRepository {
     findAll: <T extends Model>() => Promise<T[]>
     findOne: <T extends Model>(options: Partial<T>) => Promise<T>
-    create: <T extends Omit<Model, 'id'>>(data: T) => Promise<void>
+    create: <T extends Model>(data: T) => Promise<void>
     update: <T>(id: string, data: Partial<T>) => Promise<void>
     delete: (id: string) => Promise<void>
 }
