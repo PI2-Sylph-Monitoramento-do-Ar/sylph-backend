6c28058b1922d923e3b20b3ff43885b1050ad95d
GabrielDvPereira <gdavi.gd@gmail.com>
2023-02-04 17:25:22 -0300

    refactor(routes): adapt totem route to use totem controllers

--- src/routes/totem-route.ts
+++ src/routes/totem-route.ts
@@ -2,20 +2,24 @@ import { Router } from "express";
 import { adaptRoute } from "_/adapters";
 import { adaptMiddleware } from "_/adapters/middleware-adapter";
 import { COLLECTIONS } from "_/constants/colletions";
-import { TotemController } from "_/controllers/totem-controller";
+import { CreateTotemController, DeleteTotemController, FindTotemController, ListTotemController } from "_/controllers/totem-controllers";
 import { AuthMiddleware } from "_/middlewares/auth";
 import { DatabaseRepository } from "_/repositories/database";
 
 export function setTotemRoutes (router: Router){
     const totemDatabaseRepository = new DatabaseRepository(COLLECTIONS.TOTEMS)
-    const totemControtroller = new TotemController(totemDatabaseRepository)
+
+    const createTotemControtroller = new CreateTotemController(totemDatabaseRepository)
+    const deleteTotemControtroller = new DeleteTotemController(totemDatabaseRepository)
+    const findTotemControtroller = new FindTotemController(totemDatabaseRepository)
+    const ListtotemControtroller = new ListTotemController(totemDatabaseRepository)
 
     const authMiddleware = new AuthMiddleware() 
 
     // ROUTES
-    router.post('/totems', adaptRoute(totemControtroller.createTotem, totemControtroller))
-    router.delete('/totems/:totem_id', adaptMiddleware(authMiddleware), adaptRoute(totemControtroller.deleteTotem, totemControtroller))
+    router.post('/totems', adaptRoute(createTotemControtroller))
+    router.delete('/totems/:totem_id', adaptMiddleware(authMiddleware), adaptRoute(deleteTotemControtroller))
 
-    router.get('/totems', adaptRoute(totemControtroller.listTotem, totemControtroller))
-    router.get('/totems/:totem_id', adaptRoute(totemControtroller.findTotem, totemControtroller))
+    router.get('/totems', adaptRoute(ListtotemControtroller))
+    router.get('/totems/:totem_id', adaptRoute(findTotemControtroller))
 }
