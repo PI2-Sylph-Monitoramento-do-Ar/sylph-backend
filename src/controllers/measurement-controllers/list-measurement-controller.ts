c693e055ef71debfe235f721c61896d03f13800d
GabrielDvPereira <gdavi.gd@gmail.com>
2023-02-04 17:19:01 -0300

    feat(controller): add list measurement controller

---
+++ src/controllers/measurement-controllers/list-measurement-controller.ts
@@ -0,0 +1,21 @@
+import * as httpStatus from "_/helpers/http-helpers";
+import { HttpRequest, HttpResponse, Controller, IDatabaseRepository, HttpRequestParams } from "_/types";
+import { Measurement } from "_/models"
+import { FindMeasurementQuery } from './types'
+
+export class ListMeasurementController implements Controller {
+    constructor(
+        private readonly measurementDatabaseRepository: IDatabaseRepository,
+    ){}
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async handle(_: HttpRequest, httpParams: HttpRequestParams<null, FindMeasurementQuery>): Promise<HttpResponse> {
+        try {
+            const query = httpParams.query
+            const measurements = await this.measurementDatabaseRepository.findAll<Measurement>(query);
+            return httpStatus.ok(measurements)
+        } catch(error){
+            return httpStatus.serverError(error)
+        }
+    }
+}
