752a675ef165a625fafed01d9be416b161577945
GabrielDvPereira <gdavi.gd@gmail.com>
2023-02-04 17:22:56 -0300

    feat(controller): add delete totem controller

---
+++ src/controllers/totem-controllers/delete-totem-controller.ts
@@ -0,0 +1,27 @@
+import { AccessDeniedError } from "_/errors";
+import * as httpStatus from "_/helpers/http-helpers";
+import { Totem } from "_/models";
+import { HttpRequest, HttpResponse, Controller, IDatabaseRepository, HttpRequestParams } from "_/types";
+import { DeleteTotemParams, TotemHeaders } from "./types";
+
+export class DeleteTotemController implements Controller {
+    
+    constructor(private readonly totemDatabaseRepository: IDatabaseRepository){}
+
+    async handle(httpRequest: HttpRequest<null, TotemHeaders>, httpParams: HttpRequestParams<DeleteTotemParams>): Promise<HttpResponse> {
+        try {
+            const { totem_id } = httpParams.params  
+            const { email } = httpRequest.headers   
+
+            const totem = await this.totemDatabaseRepository.findOne<Totem>({id: totem_id})
+            
+            if(totem.email !== email) 
+                return httpStatus.forbidden(new AccessDeniedError())
+        
+            await this.totemDatabaseRepository.delete(totem_id)
+            return httpStatus.noContent()
+        } catch(error){
+            return httpStatus.serverError(error)
+        }
+    }
+}
