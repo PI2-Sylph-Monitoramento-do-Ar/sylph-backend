8072aa33c3fbfb4c8cfa551a88475da788c8c6b6
GabrielDvPereira <gdavi.gd@gmail.com>
2023-02-04 15:32:37 -0300

    feat(adapters): add route file adapter

---
+++ src/adapters/route-file-adapter.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express'
+import { Controller, ControllerMethod, HttpRequest, HttpRequestParams } from '_/types'
+
+/* adapter to adapt the response of express to the controller we're using.
+   it helps to treat errors in our application
+*/
+
+
+/** @TODO: 
+ * This file is too similar to adaptRoute, refactor: Template method?
+ */
+export const adaptFileRoute = (method: ControllerMethod, controller: Controller) => {
+  const bindedMethod = method.bind(controller)
+  
+  return async (req: Request, res: Response): Promise<void> => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+      headers: req.headers
+    }
+    
+    const httpRequestParams: HttpRequestParams = {
+      params: req.params, 
+      query: req.query
+    }
+
+    const httpResponse = await bindedMethod(httpRequest, httpRequestParams)
+
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+        res.set({
+            "Content-Type": "text/csv",
+            "Content-Disposition": `attachment; filename="${httpResponse.body.filename}"`,
+        }).send(httpResponse.body.data)
+    } else {
+      const { message, name, stack } = httpResponse.body as Error
+      res.status(httpResponse.statusCode).json({
+        error: {
+          message,
+          name,
+          stack
+        }
+      })
+    }
+  }
+}
